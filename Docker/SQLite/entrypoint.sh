e41c833f6f4251e211bc8c10a03baf986e42ad58
Ronald <Ronaldvr@users.noreply.github.com>
2024-04-30 15:44:09 +0200

    Update entrypoint.sh
    removed daemon mode

--- Docker/SQLite/entrypoint.sh
+++ Docker/SQLite/entrypoint.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# turn on bash's job control
-set -m
 
 # Start the primary process and put it in the background
-proftpd --nodaemon &
+#proftpd --nodaemon &
+proftpd
 
 # Start the helper process
 /usr/local/bin/docker-php-entrypoint
 
-# the my_helper_process might need to know how to wait on the
-# primary process to start before it does its work and returns
-
-
-# now we bring the primary process back into the foreground
-# and leave it there
-fg %1
