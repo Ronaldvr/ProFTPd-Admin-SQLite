0d98b86b75a12c7503b979a17e3da273f6b7110f
Ronald <Ronaldvr@users.noreply.github.com>
2024-05-06 22:18:34 +0200

    Remoce ezSQL and use PDO

--- html/includes/footer.php
+++ html/includes/footer.php
@@ -1,48 +1,48 @@
-      </div><!-- /.row -->
+</div><!-- /.row -->
 
-      <hr/>
-      <footer>
-        <center><p style="font-size:x-small">ProFTPd Admin <?php echo $ac->get_version(); ?>  is licensed under GPLv2. See <a href="https://github.com/ChristianBeer/ProFTPd-Admin">github.com/ChristianBeer/ProFTPd-Admin</a> for more information.</p></center>
-      </footer>
-    </div> <!-- /container -->
+<hr/>
+<footer>
+  <center><p style="font-size:x-small">ProFTPd Admin <?php echo $ac->get_version(); ?>  is licensed under GPLv2. See <a href="https://github.com/Ronaldvr/ProFTPd-Admin-SQLite">github.com/Ronaldvr/ProFTPd-Admin-SQLite</a> for more information.</p></center>
+</footer>
+</div> <!-- /container -->
 
-    <script src="bootstrap/js/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <script src="bootstrap/js/jqBootstrapValidation.js"></script>
-    <script src="bootstrap/js/moment.min.js"></script>
-    <script src="bootstrap/js/bootstrap-sortable.js"></script>
-    <script src="bootstrap/js/bootstrap-multiselect.js"></script>
-    <script src="bootstrap/js/bootstrap-datetimepicker.js"></script>
+<script src="bootstrap/js/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="bootstrap/js/jqBootstrapValidation.js"></script>
+<script src="bootstrap/js/moment.min.js"></script>
+<script src="bootstrap/js/bootstrap-sortable.js"></script>
+<script src="bootstrap/js/bootstrap-multiselect.js"></script>
+<script src="bootstrap/js/bootstrap-datetimepicker.js"></script>
 
-    <script type="text/javascript">
-      $(function () {
-        $('#expiration').datetimepicker({
-        useCurrent : false,
-        showClear: true,
-        format:'YYYY-MM-DD HH:mm:00',
-	minDate: 'now',
-    <?php
-      $field_expiration     = $cfg['field_expiration'];
-      if (!empty($user[$field_expiration]) && $user[$field_expiration] != '0000-00-00 00:00:00' ) { ?>
-        defaultDate: moment('<?php echo $user[$field_expiration]; ?>',"YYYY-MM-DD HH:mm:00"),
-    <?php } ?>
+<script type="text/javascript">
+$(function () {
+  $('#expiration').datetimepicker({
+  useCurrent : false,
+  showClear: true,
+  format:'YYYY-MM-DD HH:mm:00',
+minDate: 'now',
+<?php
+$field_expiration     = $cfg['field_expiration'];
+if (!empty($user[$field_expiration]) && $user[$field_expiration] != '0000-00-00 00:00:00' ) { ?>
+  defaultDate: moment('<?php echo $user[$field_expiration]; ?>',"YYYY-MM-DD HH:mm:00"),
+<?php } ?>
 
-        });
-      });
-    </script>
+  });
+});
+</script>
 
-    <script>
-      $(function () {
-        $("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
-        $(".multiselect").multiselect({
-          nonSelectedText: 'None selected',
-          inheritClass: true,
-          buttonWidth: '100%',
-          includeSelectAllOption: true,
-          enableFiltering: true,
-        });
-      } );
-    </script>
+<script>
+$(function () {
+  $("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
+  $(".multiselect").multiselect({
+    nonSelectedText: 'None selected',
+    inheritClass: true,
+    buttonWidth: '100%',
+    includeSelectAllOption: true,
+    enableFiltering: true,
+  });
+} );
+</script>
 
-  </body>
+</body>
 </html>
